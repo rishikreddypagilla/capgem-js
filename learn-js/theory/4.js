679a797d5e8b9d782dee680f3076b04db3ca30d3
RISHIK REDDY <153580995+rishikreddypagilla@users.noreply.github.com>
2025-08-26 18:03:36 +0530

    Add files via upload

---
+++ learn-js/theory/4.js
@@ -0,0 +1,64 @@
+
+/*
+
+BLOCK SCOPE
+the values which are declared in a function or condition which we can 
+call as a bloc kcannot be used outside block 
+for loop 
+while 
+functions 
+these are some examples for block scope
+
+------var is the only case it dosent have the block scope--------
+
+// function expression
+const num = function(n){
+    return n*n
+}
+num(10)
+
+a call back function is a function passed into another function as an argument
+which is invoked inside another function
+
+forEach()
+it accepts the call back function. calls the callback 
+
+map()
+it is used to create a new array from an existing array by applying some logic
+
+
+implicit return 
+in scenearios where we want to return one expression as shown 
+above we can rewrite this arrow function using parentheses instead of curly braces 
+there we have to  
+
+-------------------------------
+// filter filter out the elements in the array which match the condition 
+--------------------------------
+// find gives us the first element which matches the condition 
+-------------------------
+every()
+if every element need to check in the array with the condition (it returns boolean value )
+
+some()
+here any some of the array elements needs to satisfiy the condition 
+
+sort()
+
+slice() will create a copy of the array 
+[....arr] will aslo create the copy of array 
+
+reduce()
+will take all the values and make it a singular value 
+
+rest parameters(new)
+rest parameters in javascript allow you to represent an indefinite number of 
+arguments as an array inside a function
+
+
+
+
+*/
+
+
+
